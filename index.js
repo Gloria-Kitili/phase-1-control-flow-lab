eb0ca9b371f17743066adaf24260746fe773168a
gloria kitili <gloria.kitili@student.moringaschool.com>
2022-11-28 12:30:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+
+function scuberGreetingForFeet()
+ if(ride <= 400){
+  return 'This one is on me!'
+} else if (1999< ride && ride <2500) {
+return 'I will gladly take your thirty bucks.';
+} else {
+  return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheck(City){
+return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip (tip){
+  switch(tip){
+     case'generous':
+     return 'Thank you so much.'
+      break
+     case 'not as generous' :
+      return 'Thank you.'
+      default:
+        return 'Bye'
+
+
+  }
 }
+
+  
